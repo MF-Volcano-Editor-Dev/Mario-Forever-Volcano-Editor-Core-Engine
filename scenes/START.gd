8134b7b9196ccc62bba0b562744ddd8038ba818a
Lazy-Rabbit-2001 <2733679597@qq.com>
2024-07-07 21:23:36 +0800

    fixes

--- scenes/START.gd
+++ scenes/START.gd
@@ -1,12 +1,12 @@
 extends Node
 
 ## Entrance scene to make the game run as expected
-@export var start_scene: PackedScene = preload("res://engine/scenes/templates/level.tscn")
+@export_file("*.tscn") var start_scene: String = "res://engine/scenes/templates/level.tscn"
 
 
 func _enter_tree() -> void:
 	if start_scene:
-		get_tree().change_scene_to_packed(start_scene)
+		get_tree().change_scene_to_file.call_deferred(start_scene)
 	else:
 		OS.alert("The entrance scene is empty. The game cannot run as expected!", "Error!")
 		get_tree().quit(1)
